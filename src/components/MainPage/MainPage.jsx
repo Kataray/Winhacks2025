222917ab3acec7f4f2342e8b02f8366eafed713a
KatarinaMantay <katarinamantay@gmail.com>
2025-02-15 11:44:17 -0500

    Added popup window

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MainPage.module.css";
+import Popup from "../Popup/Popup";
 
 const MainPage = () => {
+    const [showPopup, setShowPopup] = useState(false);
+
     return (
         <div className={styles.container}>
             <h2>Insert Stuff Here</h2>
+            <button className={styles.openButton} onClick={() => setShowPopup(true)}>
+                Open Popup
+            </button>
+            {showPopup && <Popup onClose={() => setShowPopup(false)} />}
         </div>
     );
 };
