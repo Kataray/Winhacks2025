2f78872c0b201a20a35d2e48824977e1305a14e2
Yasmeen Elbik <yasmeenelbik@yahoo.com>
2025-02-15 22:55:46 -0500

    important functionality

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MainPage.module.css";
+import ChallengePage from "../Challenges/ChallengePage.jsx";
 
 const MainPage = () => {
+    const [showPopup, setShowPopup] = useState(false);
+
     return (
         <div className={styles.container}>
-            <h2>Insert Stuff Here</h2>
+            <div className={styles.ProfileSection}></div>
+            <div className={styles.ChallengeFriendSection}
+                 onClick={() => setShowPopup(true)}
+            >
+                <h2>CHALLENGES</h2>
+            </div>
+            <div className={styles.ToDoSection}></div>
+            <div className={styles.FlashcardSection}></div>
+            <div className={styles.testImage}>
+                <img src="/assets/ChopChopLogo.png" alt="Logo" className={styles.testImage}/>
+            </div>
+
+
+            {showPopup && <ChallengePage onClose={() => setShowPopup(false)}/>}
+            <div className={styles.whiteCircleBg}>
+                <img src="/assets/ProfileBgEmpty.png" alt="CircleBg" className={styles.whiteCircleBg}/>
+            </div>
+            <div className={styles.acheivmentsBox}></div>
+            <div className={styles.pointsBox}></div>
+            <div className={styles.rankBox}></div>
+
         </div>
     );
 };
